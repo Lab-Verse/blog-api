2169c6dc93df96a764b9c05dfdb901b9fd0f9fff
Muhammad Junaid Aslam <15-10648@formanite.fccollege.edu.pk>
2025-12-15 23:51:49 +0500

    fix: remove default values for database connection parameters

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -9,9 +9,9 @@ export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'postgres',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 
   // This matches all .entity.ts/.js files inside all modules/entities folders
   entities: [
